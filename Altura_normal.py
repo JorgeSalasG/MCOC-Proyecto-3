51f82d2103477016b1af9f88a89618e395720e5e
felgueta1995 <41970335+felgueta1995@users.noreply.github.com>
2018-10-28 22:37:55 -0300

    Debuggeado

--- Altura_normal.py
+++ Altura_normal.py
@@ -3,12 +3,16 @@ import scipy as sp
 #Datos Canal
 B = 10. #mts
 ss = 2. #H/V 0 si es un canal rectangula
-yini = 1. #mts
+yini = 5. #mts
 tol = 1.
 yn = yini
 C0 = 1 #unidades SI, 1,49 en caso de unidades britanicas
 S0 = 0.001 #Depende del slope 
 n = 0.013 # Depende del material
+
+q=30 #m**3/s
+
+print "Parto en: {}".format(yn)
 
 while tol>(10**(-6)):
 	#Calculamos Variables
@@ -16,18 +20,21 @@ while tol>(10**(-6)):
 	P = B + 2*yn*sp.sqrt(ss**2+1)
 	dAdyn = B + 2*ss*yn
 	dPdyn = 2*sp.sqrt(ss**2+1)
-	Q = 30. #(C0*(A**(5/3)*(P**(-2/3))*sp.sqrt(S0))/n)
+	Q = (C0*(A**(5./3.)*(P**(-2./3.))*sp.sqrt(S0))/n)
 
 	#Preparara Newton-Raphson
-	fyn = (A**(5/3))*(P**(-2/3))-(n*Q)/(C0*sp.sqrt(S0))
-	dfyn = (5/3)*(A**(2/3))*(P**(-2/3))*dAdyn - (2/3)*(A**(5/3))*(P**(-5/3))*dPdyn
+	fyn = (A**(5./3.))*(P**(-2./3.))-(n*q)/(C0*sp.sqrt(S0))
+	dfyn = (5./3.)*(A**(2./3.))*(P**(-2./3.))*dAdyn - (2./3.)*(A**(5./3.))*(P**(-5./3.))*dPdyn
 	yn2 = yn - (fyn/dfyn)
 
+
 	#Calculo tolerancia
-	tol = sp.absolute(yn2-yn)
+	tol = abs(yn2-yn)
 
 	#Se actualiza yn
 	yn = yn2
+
+	print "voy en: {}".format(yn)
 
 
 print "la altura normal es: {}".format(yn)
