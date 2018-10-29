8940cea5ef5f0a124060ffdc4c2ed56c92e1c39f
felgueta1995 <41970335+felgueta1995@users.noreply.github.com>
2018-10-28 22:38:30 -0300

    Debuggeado

--- Altura_critica.py
+++ Altura_critica.py
@@ -6,15 +6,18 @@ ss = 2. #H/V 0 si es un canal rectangula
 ycini = 1. #mts
 tol = 1.
 yc = ycini
-C0 = 1 #unidades SI, 1,49 en caso de unidades britanicas
+C0 = 1. #unidades SI, 1,49 en caso de unidades britanicas
 S0 = 0.001 #Depende del slope 
 n = 0.013 # Depende del material
 g = 9.81
-while tol>(10**(-6)):
+Q=30.
+
+c=0
+while tol>(10**(-1000)):
 	#Calculamos Variables
 	A = B*yc + ss*(yc**2)
 	P = B + 2*yc*sp.sqrt(ss**2+1)
-	Q = 30. #(C0*(A**(5/3)*(P**(-2/3))*sp.sqrt(S0))/n)
+	#Q = (C0*(A**(5./3.)*(P**(-2./3.))*sp.sqrt(S0))/n)
 	Tc = B + 2*ss*yc
 	#Preparara Newton-Raphson
 	fyc = (A**3)/(Tc)-(Q**2)/g
@@ -22,10 +25,14 @@ while tol>(10**(-6)):
 	yc2 = yc - (fyc/dfyc)
 
 	#Calculo tolerancia
-	tol = sp.absolute(yc2-yc)
+	tol = abs(yc2-yc)
 
 	#Se actualiza yc
 	yc = yc2
+	c+=1
 
-
+print c
 print "la altura critica es: {}".format(yc)
+V=Q / (B*yc + ss*(yc**2))
+fr=( V ) / sp.sqrt(g*yc)
+print "Froude es: {}".format(fr)
